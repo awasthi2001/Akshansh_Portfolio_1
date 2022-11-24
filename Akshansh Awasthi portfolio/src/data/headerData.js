ab3330ee9509d71fff2ff615833680e0bb8ca153
awasthi2001 <akshanshawasthi013@gmail.com>
2022-11-24 16:29:29 +0530

    fixing profile pic

--- Akshansh Awasthi portfolio/src/data/headerData.js
+++ Akshansh Awasthi portfolio/src/data/headerData.js
@@ -1,9 +1,9 @@
 import resume from '../assets/pdf/resume.pdf'
-
+import profilepic from '../assets/png/profilepic.png'
 export const headerData = {
     name: 'Akshansh Awasthi',
     title: "FullStack Web Developer",
     desciption:"“Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains.” - Bill Gates",
-    image: 'https://media-exp1.licdn.com/dms/image/C4E03AQFKD7ZiOb_G6A/profile-displayphoto-shrink_800_800/0/1644588252715?e=1674691200&v=beta&t=yMypQZujhZVcBV-4AIhRLc-Zg2VxAzne8AFTCk71roo',
+    image: profilepic,
     resumePdf: "https://drive.google.com/file/d/11h3Rf938YsviDqVW1WK7LNUD2bmklQVZ/view?usp=share_link"
 }
