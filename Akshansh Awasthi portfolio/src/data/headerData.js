943a5ed9c5d954a383016cc4a34d7a3b2606aa55
awasthi2001 <akshanshawasthi013@gmail.com>
2022-11-24 16:20:37 +0530

    added profile picture

--- Akshansh Awasthi portfolio/src/data/headerData.js
+++ Akshansh Awasthi portfolio/src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Akshansh Awasthi',
     title: "FullStack Web Developer",
     desciption:"“Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains.” - Bill Gates",
-    image: 'https://media-exp1.licdn.com/dms/image/C4E03AQFKD7ZiOb_G6A/profile-displayphoto-shrink_800_800/0/1644588252715?e=1669248000&v=beta&t=nQWW1q6kUkcz3E9f0UDekQa3C0oTpjOHF9wmCJQ5Dk4',
+    image: 'https://media-exp1.licdn.com/dms/image/C4E03AQFKD7ZiOb_G6A/profile-displayphoto-shrink_800_800/0/1644588252715?e=1674691200&v=beta&t=yMypQZujhZVcBV-4AIhRLc-Zg2VxAzne8AFTCk71roo',
     resumePdf: "https://drive.google.com/file/d/11h3Rf938YsviDqVW1WK7LNUD2bmklQVZ/view?usp=share_link"
 }
