71807da40bb2b7e06ec0a96757e1972d46256dd3
awasthi2001 <akshanshawasthi013@gmail.com>
2022-12-12 19:04:53 +0530

    added some changes

--- Akshansh Awasthi portfolio/src/data/contactsData.js
+++ Akshansh Awasthi portfolio/src/data/contactsData.js
@@ -3,5 +3,5 @@ export const contactsData = {
     phone: '+91 6267186126',
     address: 'Chhatarpur District Madhya Pradesh',
 
-    sheetAPI: ''
+    sheetAPI: 'https://script.google.com/macros/s/AKfycbzQh8isEji3OsYn6ye1Oy2XK8Y7l4PD2NvU8_Y0_uRvc2odenPvj7O2RXpiKbIInw07Cw/exec'
 }
