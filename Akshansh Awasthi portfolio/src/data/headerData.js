1a7e93880605aaef3c72cf130751afe542a4336a
awasthi2001 <akshanshawasthi013@gmail.com>
2023-02-04 15:01:12 +0530

    fixed image error

--- Akshansh Awasthi portfolio/src/data/headerData.js
+++ Akshansh Awasthi portfolio/src/data/headerData.js
@@ -1,10 +1,11 @@
 import resume from '../assets/pdf/resume.pdf'
 // import profilepic from '../assets/png/profilepic.png'
-// import profilepic from "../assets/png/profilepic.png"
+import profilepic from "../assets/png/profilepic.jpg"
 export const headerData = {
     name: 'Akshansh Awasthi',
     title: "FullStack Web Developer",
     desciption:"“Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains.” - Bill Gates",
-    image: "https://drive.google.com/uc?id=1-TOPZYvaXQdH9OkS6z3Ym0QidqLcBvap",
+    image: profilepic,
     resumePdf: "https://drive.google.com/file/d/11h3Rf938YsviDqVW1WK7LNUD2bmklQVZ/view?usp=share_link"
 }
+// "https://drive.google.com/uc?id=1-TOPZYvaXQdH9OkS6z3Ym0QidqLcBvap"
