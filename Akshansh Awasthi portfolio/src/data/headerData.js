d40e82950577b459cabe41aa1279067d060e0d18
awasthi2001 <akshanshawasthi013@gmail.com>
2023-02-04 14:55:03 +0530

    fixed image error

--- Akshansh Awasthi portfolio/src/data/headerData.js
+++ Akshansh Awasthi portfolio/src/data/headerData.js
@@ -5,6 +5,6 @@ export const headerData = {
     name: 'Akshansh Awasthi',
     title: "FullStack Web Developer",
     desciption:"“Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains.” - Bill Gates",
-    image: "https://media-exp1.licdn.com/dms/image/C4E03AQFKD7ZiOb_G6A/profile-displayphoto-shrink_800_800/0/1644588252715?e=1674691200&v=beta&t=yMypQZujhZVcBV-4AIhRLc-Zg2VxAzne8AFTCk71roo",
+    image: "https://drive.google.com/uc?id=1-TOPZYvaXQdH9OkS6z3Ym0QidqLcBvap",
     resumePdf: "https://drive.google.com/file/d/11h3Rf938YsviDqVW1WK7LNUD2bmklQVZ/view?usp=share_link"
 }
